b4871d6f658f275081643d3cab257c53e69970bb
arwidmark <arwidmark@gmail.com>
2022-04-22 10:23:11 -0500

    Added

---
+++ DO and BITS efficiency all Subnets/Get-StiflerRSubnetBCEfficiencyInfo.ps1
@@ -0,0 +1,42 @@
+﻿$Date = $(get-date -f MMddyyyy_hhmm)
+$DateForCSVTimeStamp = $(Get-Date -UFormat "%D %T")
+$ExportPath = "E:\2Pint_Installation\HealthCheck\Results\BITSNetworkInfo_$Date.csv"
+
+$Subnets = Get-WmiObject -Namespace root\stifler -query "Select * from Subnets where clients > 0 and VPN = 'False'"
+
+[System.Collections.ArrayList]$BITSNetworkInfo = @()
+foreach ($Subnet in $Subnets){
+    $BITSSourceHistory = $Subnet.BITSSourceHistory
+    $BITSSourceHistoryInGB = $([math]::Round(($BITSSourceHistory/1GB),2))
+    
+    $BITSPeerToPeerHistory = $Subnet.BITSPeerToPeerHistory
+    $BITSPeerToPeerHistoryInGB = $([math]::Round(($BITSPeerToPeerHistory/1GB),2))
+
+    If ($BITSPeerToPeerHistory -eq 0){
+        $BITSEfficiency = 0
+    }
+    Else {
+        $BITSEfficiency = $BITSPeerToPeerHistory/($BITSSourceHistory + $BITSPeerToPeerHistory)
+        $BITSEfficiency = $([math]::Round(($BITSEfficiency*100),2))
+        
+    }
+
+    $obj = [PSCustomObject]@{
+
+        # Add values to arraylist
+        SubnetID  =  $Subnet.SubnetID
+        DataCollectDate = $DateForCSVTimeStamp
+        Clients = $Subnet.clients
+        BITSSourceHistoryInGB = $BITSSourceHistoryInGB
+        BITSPeerToPeerHistoryInGB = $BITSPeerToPeerHistoryInGB
+        BITSEfficiency = $BITSEfficiency 
+        ID = $subnet.id
+    }
+
+    # Add all the values
+    $BITSNetworkInfo.Add($obj)|Out-Null
+
+
+}
+
+$BITSNetworkInfo | Export-Csv -Path $ExportPath -NoTypeInformation
---
+++ DO and BITS efficiency all Subnets/Get-StiflerRSubnetDOEfficiencyInfo.ps1
@@ -0,0 +1,42 @@
+﻿$Date = $(get-date -f MMddyyyy_hhmm)
+$DateForCSVTimeStamp = $(Get-Date -UFormat "%D %T")
+$ExportPath = "E:\2Pint_Installation\HealthCheck\Results\DONetworkInfo_$Date.csv"
+
+$Subnets = Get-WmiObject -Namespace root\stifler -query "Select * from Subnets where clients > 0 and VPN = 'False'"
+
+[System.Collections.ArrayList]$DONetWorkInfo = @()
+foreach ($Subnet in $Subnets){
+    $DOSourceHistory = $Subnet.DOSourceHistory
+    $DOSourceHistoryInGB = $([math]::Round(($DOSourceHistory/1GB),2))
+    
+    $DOPeerToPeerHistory = $Subnet.DOPeerToPeerHistory
+    $DOPeerToPeerHistoryInGB = $([math]::Round(($DOPeerToPeerHistory/1GB),2))
+
+    If ($DOPeerToPeerHistory -eq 0){
+        $DOEfficiency = 0
+    }
+    Else {
+        $DOEfficiency = $DOPeerToPeerHistory/($DOSourceHistory + $DOPeerToPeerHistory)
+        $DOEfficiency = $([math]::Round(($DOEfficiency*100),2))
+        
+    }
+
+    $obj = [PSCustomObject]@{
+
+        # Add values to arraylist
+        SubnetID  =  $Subnet.SubnetID
+        DataCollectDate = $DateForCSVTimeStamp
+        Clients = $Subnet.clients
+        DOSourceHistoryInGB = $DOSourceHistoryInGB
+        DOPeerToPeerHistoryInGB = $DOPeerToPeerHistoryInGB
+        DOEfficiency = $DOEfficiency 
+        ID = $subnet.id
+    }
+
+    # Add all the values
+    $DONetWorkInfo.Add($obj)|Out-Null
+
+
+}
+
+$DONetWorkInfo | Export-Csv -Path $ExportPath -NoTypeInformation
