89ed49246b7097a85149a79d26f83077316b0b35
arwidmark <arwidmark@gmail.com>
2023-12-15 10:38:42 -0600

    Create StiflerClientDetectionScriptForConfigMgr.ps1

---
+++ InstallerScripts/Client/StiflerClientDetectionScriptForConfigMgr.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = "SilentlyContinue"
+
+# Stifler MSI
+$RegistryPathStiflerMSIx64 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{132A87D5-C9E9-4BBA-A801-7B64986AA448}'
+$RegistryPathStiflerMSIx86 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{F418D102-42EA-4BAC-B979-F785981AADDF}'
+
+# StifleR Service
+$serviceStatus = $False
+$serviceName = "StifleRClient"
+$Installed = $False
+
+$service = Get-Service -Name $serviceName
+
+if ($service.Status -eq "Running")
+{
+    $serviceStatus = $True
+    
+}
+else
+{
+    $serviceStatus = $False
+}
+
+
+if (((Test-Path -Path $RegistryPathStiflerMSIx64) -or (Test-Path -Path $RegistryPathStiflerMSIx86)) -and ($serviceStatus -eq $True)) 
+{
+    write-host "Installed"
+    
+}
+else 
+{
+
+}
