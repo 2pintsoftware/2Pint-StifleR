9ed871a328e37f7fae37950bf4aca9498142e59a
arwidmark <arwidmark@gmail.com>
2023-12-20 11:05:46 -0600

    Create UninstallStiflerClient.cmd

---
+++ InstallerScripts/Client/UninstallStiflerClient.cmd
@@ -0,0 +1,11 @@
+REM - this CMD file checks the platform (x86/64) and then runs the correct PS command line
+
+
+PUSHD %~dp0
+If "%PROCESSOR_ARCHITEW6432%"=="AMD64" GOTO 64bit
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command ".\StifleR_Client_Installer.ps1 -Uninstall $true"
+GOTO END
+:64bit
+"%WinDir%\Sysnative\windowsPowershell\v1.0\Powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\StifleR_Client_Installer.ps1 -Uninstall $true"
+:END
+POPD
