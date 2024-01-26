cb8757a1c0fdc6a1f541cfc4ae2bde3da0d2deff
Mattias Benninge <15101419+matbe@users.noreply.github.com>
2024-01-26 19:35:33 +0100

    Enable-StiflerDebugEventlogs.ps1 : Added support for Stifler Server debug logs as well

--- Troubleshoot/Enable-StiflerDebugEventlogs.ps1
+++ Troubleshoot/Enable-StiflerDebugEventlogs.ps1
@@ -37,68 +37,115 @@ Param (
     [Switch]$ResetLogMAXSize
 )
 
+$IsStiflerServer = $false
+$IsStiflerClient = $false
 
+if(Get-service -name StifleRServer -ErrorAction SilentlyContinue) {
+    $IsStiflerServer = $true
+}
 
-if($enableDebugLogs -and $disableDebugLogs)
-{
+if (Get-Service -Name StifleRClient -ErrorAction SilentlyContinue) {
+    $IsStiflerClient = $true
+}
+
+If(!$IsStiflerServer -and !$IsStiflerClient) {
+    Write-error "No Stifler installed in this machine"
+    break
+}
+
+if ($enableDebugLogs -and $disableDebugLogs) {
     Write-Warning "You can't both enable and disable the eventlogs!! Make up your mind please!"
     break
 }
 
-if($SetLogMAXSize -and $ResetLogMAXSize)
-{
+if ($SetLogMAXSize -and $ResetLogMAXSize) {
     Write-Warning "You can't both change and reset the eventlogs size!! Make up your mind please!"
     break
 }
 
-$debuglogfiles = @(
-"TwoPintSoftware-StifleR.ClientApp-Bandwidth/Analytic",
-"TwoPintSoftware-StifleR.ClientApp-Bandwidth/Debug",
-"TwoPintSoftware-StifleR.ClientApp-DeliveryOptimization/Debug",
-"TwoPintSoftware-StifleR.ClientApp-Jobs/Debug",
-"TwoPintSoftware-StifleR.ClientApp-Leader/Debug",
-"TwoPintSoftware-StifleR.ClientApp-Location/Analytic",
-"TwoPintSoftware-StifleR.ClientApp-Location/Debug",
-"TwoPintSoftware-StifleR.ClientApp-MainLoop/Debug",
-"TwoPintSoftware-StifleR.ClientApp-Program/Debug",
-"TwoPintSoftware-StifleR.ClientApp-SignalR/Debug",
-"TwoPintSoftware-StifleR.ClientApp-TriggersAndEvents/Debug",
-"TwoPintSoftware-StifleR.ClientApp-TypeDetection/Debug"
+$serverdebuglogfiles = @(
+    "TwoPintSoftware-StifleR.Service-BandwidthWatchdog/Debug",
+    "TwoPintSoftware-StifleR.Service-DataEngine/Analytic",
+    "TwoPintSoftware-StifleR.Service-DataEngine/Debug",
+    "TwoPintSoftware-StifleR.Service-LocationService/Debug",
+    "TwoPintSoftware-StifleR.Service-Leaders/Analytic",
+    "TwoPintSoftware-StifleR.Service-Leaders/Debug",
+    "TwoPintSoftware-StifleR.Service-LocationService/Admin",
+    "TwoPintSoftware-StifleR.Service-Security/Admin",
+    "TwoPintSoftware-StifleR.Service-Security/Debug",
+    "TwoPintSoftware-StifleR.Service-SignalR/Debug",
+    "TwoPintSoftware-StifleR.Service-StifleREngine/Debug",
+    "TwoPintSoftware-StifleR.Service-WebApi/Admin",
+    "TwoPintSoftware-StifleR.Service-WebApi/Debug",
+    "TwoPintSoftware-StifleR.Service-WinService/Debug"
 )
 
-$standardlogfiles = @(
-"TwoPintSoftware-StifleR.ClientApp-BITSBranchCache/Operational",
-"TwoPintSoftware-StifleR.ClientApp-Bandwidth/Operational",
-"TwoPintSoftware-StifleR.ClientApp-DeliveryOptimization/Operational",
-"TwoPintSoftware-StifleR.ClientApp-Jobs/Operational",
-"TwoPintSoftware-StifleR.ClientApp-Leader/Operational",
-"TwoPintSoftware-StifleR.ClientApp-Location/Operational",
-"TwoPintSoftware-StifleR.ClientApp-MainLoop/Operational",
-"TwoPintSoftware-StifleR.ClientApp-Program/Operational",
-"TwoPintSoftware-StifleR.ClientApp-SignalR/Operational",
-"TwoPintSoftware-StifleR.ClientApp-SignalRMessages/Operational",
-"TwoPintSoftware-StifleR.ClientApp-TSHelper/Operational",
-"TwoPintSoftware-StifleR.ClientApp-TriggersAndEvents/Operational",
-"TwoPintSoftware-StifleR.ClientApp-TypeDetection/Operational"
+$serverstandardlogfiles = @(
+    "TwoPintSoftware-StifleR.Service-BandwidthWatchdog/Operational",
+    "TwoPintSoftware-StifleR.Service-DataEngine/Operational",
+    "TwoPintSoftware-StifleR.Service-Leaders/Operational",
+    "TwoPintSoftware-StifleR.Service-LocationService/Operational",
+    "TwoPintSoftware-StifleR.Service-Security/Operational",
+    "TwoPintSoftware-StifleR.Service-SignalR/Operational",
+    "TwoPintSoftware-StifleR.Service-StifleREngine/Operational",
+    "TwoPintSoftware-StifleR.Service-WebApi/Operational",
+    "TwoPintSoftware-StifleR.Service-WinService/Operational",
+    "TwoPintSoftware-StifleR.Service-WMI/Operational"
 )
 
+$clientdebuglogfiles = @(
+    "TwoPintSoftware-StifleR.ClientApp-Bandwidth/Analytic",
+    "TwoPintSoftware-StifleR.ClientApp-Bandwidth/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-DeliveryOptimization/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-Jobs/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-Leader/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-Location/Analytic",
+    "TwoPintSoftware-StifleR.ClientApp-Location/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-MainLoop/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-Program/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-SignalR/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-TriggersAndEvents/Debug",
+    "TwoPintSoftware-StifleR.ClientApp-TypeDetection/Debug"
+)
+
+$clientstandardlogfiles = @(
+    "TwoPintSoftware-StifleR.ClientApp-BITSBranchCache/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-Bandwidth/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-DeliveryOptimization/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-Jobs/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-Leader/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-Location/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-MainLoop/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-Program/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-SignalR/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-SignalRMessages/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-TSHelper/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-TriggersAndEvents/Operational",
+    "TwoPintSoftware-StifleR.ClientApp-TypeDetection/Operational"
+)
+
+if ($IsStiflerServer) {
+    $debuglogfiles += $serverdebuglogfiles
+    $standardlogfiles += $serverstandardlogfiles 
+}
+if ($IsStiflerClient) {
+    $debuglogfiles += $clientdebuglogfiles
+    $standardlogfiles += $clientstandardlogfiles
+}
+ 
 $logfiles = $debuglogfiles + $standardlogfiles
 
-$debugLogsMax = 20
+$debugLogsMax = 28
 
-function Set-EventLogSize($eventLogNames, $SetLogMAXSize)
-{
-    foreach ($eventLogName in $eventLogNames)
-    {
+function Set-EventLogSize($eventLogNames, $SetLogMAXSize) {
+    foreach ($eventLogName in $eventLogNames) {
         $error.clear()
 
-        try
-        {
+        try {
             $session = New-Object Diagnostics.Eventing.Reader.EventLogSession ($machine)
-            $eventLog = New-Object Diagnostics.Eventing.Reader.EventLogConfiguration ($eventLogName,$session)
+            $eventLog = New-Object Diagnostics.Eventing.Reader.EventLogConfiguration ($eventLogName, $session)
         }
-        catch
-        {
+        catch {
             Write-host "warning:unable to open eventlog $($eventLogName) $($error)"
             $error.clear()
         }
@@ -113,46 +160,37 @@ function Set-EventLogSize($eventLogNames, $SetLogMAXSize)
     }
 }
 
-if($SetLogMAXSize)
-{
+if ($SetLogMAXSize) {
     Set-EventLogSize -eventLogNames $logfiles -SetLogMAXSize $SetLogMAXSize
 }
 
-if($ResetLogMAXSize)
-{
+if ($ResetLogMAXSize) {
     Set-EventLogSize -eventLogNames $logfiles -SetLogMAXSize 1052672 # 1028Kb
 }
 
-function enable-logs($eventLogNames)
-{
+function enable-logs($eventLogNames) {
     Write-host "enabling / disabling logs on localhost)."
     [Text.StringBuilder] $sb = new-object Text.StringBuilder
     $debugLogsEnabled = New-Object Collections.ArrayList
     [void]$sb.Appendline("event logs:")
 
-    try
-    {
-        foreach ($eventLogName in $eventLogNames)
-        {
+    try {
+        foreach ($eventLogName in $eventLogNames) {
             $error.clear()
 
-            try
-            {
+            try {
                 $session = New-Object Diagnostics.Eventing.Reader.EventLogSession ($machine)
-                $eventLog = New-Object Diagnostics.Eventing.Reader.EventLogConfiguration ($eventLogName,$session)
+                $eventLog = New-Object Diagnostics.Eventing.Reader.EventLogConfiguration ($eventLogName, $session)
             }
-            catch
-            {
+            catch {
                 Write-host "warning:unable to open eventlog $($eventLogName) $($error)"
                 $error.clear()
             }
 
-            if ($clearEventLogs)
-            {
+            if ($clearEventLogs) {
                 [void]$sb.AppendLine("clearing event log: $($eventLogName)")
             
-                if ($eventLog.IsEnabled -and !$eventLog.IsClassicLog)
-                {
+                if ($eventLog.IsEnabled -and !$eventLog.IsClassicLog) {
                     $eventLog.IsEnabled = $false
                     $eventLog.SaveChanges()
                     $eventLog.Dispose()
@@ -163,50 +201,41 @@ function enable-logs($eventLogNames)
                     $eventLog.IsEnabled = $true
                     $eventLog.SaveChanges()
                 }
-                elseif ($eventLog.IsClassicLog)
-                {
+                elseif ($eventLog.IsClassicLog) {
                     $session.ClearLog($eventLogName)
                 }
             }
 
-            if ($enableDebugLogs -and $eventLog.IsEnabled -eq $false)
-            {
-                if ($VerbosePreference -ine "SilentlyContinue" -or $listEventLogs)
-                {
+            if ($enableDebugLogs -and $eventLog.IsEnabled -eq $false) {
+                if ($VerbosePreference -ine "SilentlyContinue" -or $listEventLogs) {
                     [void]$sb.AppendLine("enabling debug log for $($eventLog.LogName) $($eventLog.LogMode)")
                 }
          
                 $eventLog.IsEnabled = $true
                 $eventLog.SaveChanges()
-                $global:debugLogsCount++
+                #$global:debugLogsCount++
             }
 
-            if ($disableDebugLogs -and $eventLog.IsEnabled -eq $true -and ($eventLog.LogType -ieq "Analytic" -or $eventLog.LogType -ieq "Debug"))
-            {
-                if ($VerbosePreference -ine "SilentlyContinue" -or $listEventLogs)
-                {
+            if ($disableDebugLogs -and $eventLog.IsEnabled -eq $true -and ($eventLog.LogType -ieq "Analytic" -or $eventLog.LogType -ieq "Debug")) {
+                if ($VerbosePreference -ine "SilentlyContinue" -or $listEventLogs) {
                     [void]$sb.AppendLine("disabling debug log for $($eventLog.LogName) $($eventLog.LogMode)")
                 }
 
                 $eventLog.IsEnabled = $false
                 $eventLog.SaveChanges()
-                $global:debugLogsCount--
+                #$global:debugLogsCount--
 
-                if ($debugLogsEnabled.Contains($eventLog.LogName))
-                {
+                if ($debugLogsEnabled.Contains($eventLog.LogName)) {
                     $debugLogsEnabled.Remove($eventLog.LogName)
                 }
             }
 
-            if ($eventLog.LogType -ieq "Analytic" -or $eventLog.LogType -ieq "Debug")
-            {
-                if ($eventLog.IsEnabled -eq $true)
-                {
+            if ($eventLog.LogType -ieq "Analytic" -or $eventLog.LogType -ieq "Debug") {
+                if ($eventLog.IsEnabled -eq $true) {
                     [void]$sb.AppendLine("$($eventLog.LogName) $($eventLog.LogMode): ENABLED")
-                    $debugLogsEnabled.Add($eventLog.LogName)
+                    $debugLogsEnabled.Add($eventLog.LogName) | Out-Null
 
-                    if ($debugLogsMax -le $debugLogsEnabled.Count)
-                    {
+                    if ($debugLogsMax -le $debugLogsEnabled.Count) {
                         Write-host "Error: too many debug logs enabled ($($debugLogsMax))."
                         Write-host "Error: this can cause system performance / stability issues as well as inability to boot!"
                         Write-host "Error: rerun script again with these switches: .\event-log-manager.ps1 -listeventlogs -disableDebugLogs"
@@ -215,13 +244,11 @@ function enable-logs($eventLogNames)
                         exit 1
                     }
                 }
-                else
-                {
+                else {
                     [void]$sb.AppendLine("$($eventLog.LogName) $($eventLog.LogMode): DISABLED")
                 }
             }
-            else
-            {
+            else {
                 [void]$sb.AppendLine("$($eventLog.LogName)")
             }
         }
@@ -229,10 +256,8 @@ function enable-logs($eventLogNames)
         Write-host $sb.ToString()
         Write-host "-----------------------------------------"
 
-        if ($debugLogsEnabled.Count -gt 0)
-        {
-            foreach ($eventLogName in $debugLogsEnabled)
-            {
+        if ($debugLogsEnabled.Count -gt 0) {
+            foreach ($eventLogName in $debugLogsEnabled) {
                 Write-host $eventLogName
             }
 
@@ -240,18 +265,15 @@ function enable-logs($eventLogNames)
         }
 
     }
-    catch
-    {
+    catch {
         Write-host "enable logs exception: $($error | out-string)"
         $error.Clear()
     }
 }
 
-if($enableDebugLogs -or $disableDebugLogs)
-{
+if ($enableDebugLogs -or $disableDebugLogs) {
     enable-logs -eventLogNames $debuglogfiles
 }
-if($clearEventLogs)
-{
+if ($clearEventLogs) {
     enable-logs -eventLogNames $logfiles
 }
