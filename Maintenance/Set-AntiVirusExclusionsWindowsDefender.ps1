66821bc64544dbca79e5dd049c5057948cdac122
arwidmark <arwidmark@gmail.com>
2024-07-08 10:08:19 -0500

    Create Set-AntiVirusExclusionsWindowsDefender.ps1

---
+++ Maintenance/Set-AntiVirusExclusionsWindowsDefender.ps1
@@ -0,0 +1,24 @@
+﻿# Sample StifleR Server exclusion rules for Windows Defender
+
+# Change the below to match your environment
+$ProcessName = "StifleR.Service"
+$StiflerDBPath = "E:\StifleRDB"
+$SQLHistoryDBPath = "E:\SQLDB"
+
+# Exclude StifleR Process
+$ProcessPath = (Get-Process -Name $ProcessName ).Path
+Add-MpPreference -ExclusionProcess $ProcessPath
+
+# Exclude installation folders
+$InstallationFolder = Split-Path $ProcessPath -Parent
+Add-MpPreference -ExclusionPath $InstallationFolder
+
+# Exclude database folders
+Add-MpPreference -ExclusionPath $SQLHistoryDBPath 
+Add-MpPreference -ExclusionPath $StiflerDBPath
+
+# Show exclusions
+(Get-MpPreference).ExclusionPath
+(Get-MpPreference).ExclusionProcess
+
+
