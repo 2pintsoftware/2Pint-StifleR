b89a7a09a28c7ef7fb4f1c4adc292fa83e66c4e7
arwidmark <arwidmark@gmail.com>
2024-07-08 10:22:26 -0500

    Added

---
+++ Maintenance/Set-2PXEAntiVirusExclusionsWindowsDefender.ps1
@@ -0,0 +1,20 @@
+﻿# Sample 2PXE exclusion rules for Windows Defender
+
+# Change the below to match your environment
+$ProcessName = "2Pint.2pxe.Service"
+
+# Exclude StifleR Process
+$ProcessPath = (Get-Process -Name $ProcessName ).Path
+Add-MpPreference -ExclusionProcess $ProcessPath
+
+# Exclude installation folder
+$InstallationFolder = Split-Path $ProcessPath -Parent
+Add-MpPreference -ExclusionPath $InstallationFolder
+
+# Exclude 2PXE program data folder
+Add-MpPreference -ExclusionPath "C:\ProgramData\2Pint Software\2PXE"
+
+# Show exclusions
+(Get-MpPreference).ExclusionPath
+(Get-MpPreference).ExclusionProcess
+
---
+++ Maintenance/Set-iPXEWSAntiVirusExclusionsWindowsDefender.ps1
@@ -0,0 +1,24 @@
+﻿# Sample iPXE Anywhere exclusion rules for Windows Defender
+
+# Change the below to match your environment
+$ProcessName = "iPXEAnywhere.Service"
+$SQLDBPath = "E:\SQLDB"
+
+# Exclude StifleR Process
+$ProcessPath = (Get-Process -Name $ProcessName ).Path
+Add-MpPreference -ExclusionProcess $ProcessPath
+
+# Exclude installation folder
+$InstallationFolder = Split-Path $ProcessPath -Parent
+Add-MpPreference -ExclusionPath $InstallationFolder
+
+# Exclude 2PXE program data folder
+Add-MpPreference -ExclusionPath "C:\ProgramData\2Pint Software\iPXEWS"
+
+# Exclude database folder
+Add-MpPreference -ExclusionPath $SQLDBPath 
+
+# Show exclusions
+(Get-MpPreference).ExclusionPath
+(Get-MpPreference).ExclusionProcess
+
